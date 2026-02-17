687b09acc07aceaed70084b35cec3c231cf62fef
dandanCod123 <danielmouradesousa5@gmail.com>
2026-02-17 10:44:25 -0300

    chore: add next-shacdn-database-prettier-eslint setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>ola undo</h1>
+      <Card className="bg-amber-300">test</Card>
+    </div>
+  );
+}
