53183ff55bff97a059275660d43732c8067ec5de
dandanCod123 <danielmouradesousa5@gmail.com>
2026-02-17 19:18:51 -0300

    feat:add custom errom message

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,11 +1,18 @@
-"use client";
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import SignUpForm from "./components/sign-up-form";
 import LoginForm from "./components/login-form";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-function AuthenticationPage() {
+const AuthenticationPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session?.user) {
+    redirect("/dashboard");
+  }
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Tabs defaultValue="login" className="w-[400px]">
@@ -22,6 +29,6 @@ function AuthenticationPage() {
       </Tabs>
     </div>
   );
-}
+};
 
 export default AuthenticationPage;
