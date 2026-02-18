d4e0d62ea73ba878673d9c6085cf8ae285e5d81a
dandanCod123 <danielmouradesousa5@gmail.com>
2026-02-18 13:24:16 -0300

    feat:create Doctors page and container components

---
+++ src/app/(protected)/doctors/page.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+import { Plus } from "lucide-react";
+
+const DoctorsPage = () => {
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>
+            Access a detailed overview of key metrics and patient outcomes
+          </PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <Button>
+            <Plus />
+            Adicionar Médico
+          </Button>
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <h1>Medicos</h1>
+      </PageContent>
+    </PageContainer>
+  );
+};
+
+export default DoctorsPage;
