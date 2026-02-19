0223cf9798f8b6c6350237e116ee5da9a847d66a
dandanCod123 <danielmouradesousa5@gmail.com>
2026-02-19 18:38:36 -0300

    feat:ensure user has clinic on doctors page

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -8,9 +8,27 @@ import {
   PageHeaderContent,
   PageTitle,
 } from "@/components/ui/page-container";
+import { db } from "@/db";
+import { doctorsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+import { eq } from "drizzle-orm";
 import { Plus } from "lucide-react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const DoctorsPage = () => {
+const DoctorsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+  if (!session.user.clinic) {
+    redirect("/clinic-form");
+  }
+  const doctors = await db.query.doctorsTable.findMany({
+    where: eq(doctorsTable.clinicId, session.user.clinic.id),
+  });
   return (
     <PageContainer>
       <PageHeader>
