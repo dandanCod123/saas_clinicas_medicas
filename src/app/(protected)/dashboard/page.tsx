9d659f8778eaf10d489944024e020f235b33f40d
dandanCod123 <danielmouradesousa5@gmail.com>
2026-02-25 11:06:04 -0300

    feat: add cards dashboard and urls verification

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,12 +1,16 @@
-import { eq } from "drizzle-orm";
+import { and, count, eq, gte, lte, sum } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { db } from "@/db";
-import { usersToClinicsTable } from "@/db/schema";
+import {
+  appointmentsTable,
+  doctorsTable,
+  patientsTable,
+  usersToClinicsTable,
+} from "@/db/schema";
 import { auth } from "@/lib/auth";
 
-import { DataTable } from "@/components/ui/data-table";
 import {
   PageContainer,
   PageHeader,
@@ -16,30 +20,87 @@ import {
   PageActions,
   PageContent,
 } from "@/components/ui/page-container";
-import AddPatientButton from "../patients/_components/add-patient-button";
-import { PatiensTableColumns } from "../patients/_components/table-columns";
-import { DatePicker } from "./components/date-picker";
 
-const DashboardPage = async () => {
+import { DatePicker } from "./components/date-picker";
+import StatsCards from "./components/stats-cards";
+import dayjs from "dayjs";
+
+interface DashboardPageProps {
+  searchParams: Promise<{
+    from: string;
+    to: string;
+  }>;
+}
+
+const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
+  const { from, to } = await searchParams;
   const session = await auth.api.getSession({
     headers: await headers(),
   });
   if (!session?.user) {
     redirect("/authentication");
   }
-  const clinics = await db.query.usersToClinicsTable.findMany({
-    where: eq(usersToClinicsTable.userId, session.user.id),
-  });
-  if (clinics.length === 0) {
+
+  if (!session.user.clinic) {
     redirect("/clinic-form");
   }
+  //AQUI NESSSA PARTE VAI TER QUE SER FEITO UMA VERIFICAÇÃO
+  // SE EXISTE O FROM E O TO, SE NÃO EXISTIR VAI REDIRECIONAR
+  // PARA MESMA ROTA PASSANDO O FROM COMO DATA ATUAL E O TO COMO
+  // DATA ATUAL + 1 MES, ISSO VAI GARANTIR QUE SEMPRE TENHA UM INTERVALO
+  // DE DATA PARA SER FEITO AS CONSULTAS NO BANCO DE DADOS, POIS O DASHBOARD
+  // DEPENDE DISSO PARA FUNCIONAR
+  if (!from || !to) {
+    redirect(
+      `/dashboard?from=${dayjs().format("YYYY-MM-DD")}&to=${dayjs().add(1, "month").format("YYYY-MM-DD")}`,
+    );
+  }
+  const [[totalRevenue], [totalAppointments], [totalPatients], [totalDoctors]] =
+    await Promise.all([
+      db
+        .select({
+          total: sum(appointmentsTable.appointmentPriceInCents),
+        })
+        .from(appointmentsTable)
+        .where(
+          and(
+            eq(appointmentsTable.clinicId, session.user.clinic.id),
+            gte(appointmentsTable.date, new Date(from)),
+            lte(appointmentsTable.date, new Date(to)),
+          ),
+        ),
+      db
+        .select({
+          total: count(),
+        })
+        .from(appointmentsTable)
+        .where(
+          and(
+            eq(appointmentsTable.clinicId, session.user.clinic.id),
+            gte(appointmentsTable.date, new Date(from)),
+            lte(appointmentsTable.date, new Date(to)),
+          ),
+        ),
+      db
+        .select({
+          total: count(),
+        })
+        .from(patientsTable)
+        .where(eq(patientsTable.clinicId, session.user.clinic.id)),
+      db
+        .select({
+          total: count(),
+        })
+        .from(doctorsTable)
+        .where(eq(doctorsTable.clinicId, session.user.clinic.id)),
+    ]);
   return (
     <PageContainer>
       <PageHeader>
         <PageHeaderContent>
-          <PageTitle>Pacientes</PageTitle>
+          <PageTitle>Dashboard</PageTitle>
           <PageDescription>
-            Acesse uma visão detalhada de todos os seus pacientes
+            Access a detailed overview of key metrics and patient outcomes
           </PageDescription>
         </PageHeaderContent>
         <PageActions>
@@ -49,7 +110,12 @@ const DashboardPage = async () => {
         </PageActions>
       </PageHeader>
       <PageContent>
-        <></>
+        <StatsCards
+          totalRevenue={totalRevenue.total ? Number(totalRevenue.total) : null}
+          totalAppointments={totalAppointments.total}
+          totalPatients={totalPatients.total}
+          totalDoctors={totalDoctors.total}
+        />
       </PageContent>
     </PageContainer>
   );
